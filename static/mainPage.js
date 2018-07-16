b4461ab2f8208b1915ec95a04ad5dd1020c7c463
spainhour <msspainhour@gmail.com>
2018-07-16 11:40:07 -0500

    drag and drop

--- static/mainPage.js
+++ static/mainPage.js
@@ -1,3 +1,12 @@
+var id;
 function allowDrop(event) {
   event.preventDefault();
+}
+
+function dragStart(event) {
+  id=event.target.id;
+}
+
+function drop(event) {
+  event.target.append(document.getElementById(id));
 }
