4ddfea935e4abcc525e57c7274a34555b5ab0b74
spainhour <msspainhour@gmail.com>
2018-08-04 17:29:01 -0500

    Tool box creates a clone on click, gives an endless supply of "furniture"

--- static/setup.js
+++ static/setup.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-  $('.element').draggable({ containment: '#dropzone', cursor: 'pointer'});
-  $('#dropzone').droppable()
+  $('.element').draggable({ containment: '#dropzone', cursor: 'pointer', helper: 'clone', revert: 'invalid'});
+});
+
+$("#dropzone").droppable({
+    accept: ".element",
+    drop: function(event,ui){
+        var new_signature = $(ui.helper).clone().removeClass('element');
+        new_signature.draggable();
+        $(this).append(new_signature).attr("id", )
+    }
 });
