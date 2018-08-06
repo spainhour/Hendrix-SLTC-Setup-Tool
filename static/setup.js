460e7d7330d442e2d3769f1acae7ba6353e20a06
spainhour <msspainhour@gmail.com>
2018-08-06 00:40:20 -0500

    DOMs rotate

--- static/setup.js
+++ static/setup.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
-  $('.element').draggable({ containment: '#dropzone', cursor: 'pointer', helper: 'clone', revert: 'invalid'});
+  $('.brown').draggable({containment: '#dropzone', cursor: 'pointer', helper: 'clone', revert: 'invalid'});
+  $('.grey').draggable({containment: '#dropzone', cursor: 'pointer', helper: 'clone', revert: 'invalid'});
+  $('.round').draggable({containment: '#dropzone', cursor: 'pointer', helper: 'clone', revert: 'invalid'});
 });
 
 $("#dropzone").droppable({
-    accept: ".element",
-    drop: function(event,ui){
-        var new_signature = $(ui.helper).clone().removeClass('element');
-        new_signature.draggable();
-        $(this).append(new_signature).attr("id", )
-    }
+  accept: ".brown, .grey, .round",
+  drop: function(event,ui){
+      var new_signature = $(ui.helper).clone();
+      new_signature.draggable({containment: '#dropzone', cursor: 'pointer', revert: 'invalid'});
+      $(this).append(new_signature);
+      $(ui.helper).remove();
+      $('.brown, .grey, .round').click(function() {
+          rotation += 45;
+          $(this).rotate(rotation);
+      });
+  }
 });
+
+var rotation = 0;
+jQuery.fn.rotate = function(degrees) {
+    $(this).css({'transform' : 'rotate('+ degrees +'deg)'});
+    return $(this);
+};
